1532cfb4ea237133b966b434dd4726feb21a752d
iraqoze <iraqoze.lionel>
2021-12-04 01:21:30 +0300

    Commit

---
+++ Arrays/Arrays/Array.cs
@@ -0,0 +1,85 @@
+using System;
+
+namespace Arrays
+{
+    class Array
+    {
+        private int[] array;
+        private int Count = 0;
+        public Array(int lenght)
+        {
+            array = new int[lenght];
+        }
+
+        //Inserting values into the array
+        public void Insert(int value)
+        {
+            if (!IsArrayFull())
+                array[Count++] = value;
+            else
+                throw new Exception("The Array Is Full");
+        }
+
+        //Get value at a given Index
+        public int ElementAt(int index)
+        {
+            if (IsValidIndex(index))
+            {
+                return array[index];
+            }
+            throw new Exception("Invalid Index");
+
+        }
+
+        //Remove At
+        public void RemoveAt(int index)
+        {
+            if (!IsValidIndex(index))
+                throw new Exception("Invalid Index");
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            Count--;
+            array[Count] = 0;
+        }
+
+        //Index Of
+
+        public int IndexOf(int value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == value)
+                    return i;
+            }
+            return -1;
+        }
+        //Checking if the array is full
+        private bool IsArrayFull()
+        {
+            if (Count == array.Length)
+                return true;
+            return false;
+        }
+
+        //Validate index
+        private bool IsValidIndex(int index)
+        {
+            if (index >= 0 && index <= array.Length - 1)
+                return true;
+            return false;
+        }
+
+        //Printing the array
+        public void PrintArray()
+        {
+            for (int i = 0; i < Count; i++)
+                Console.WriteLine(array[i]);
+
+        }
+
+    }
+}
--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -6,7 +6,28 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Array array = new Array(8);
+            array.Insert(10);
+            array.Insert(20);
+            array.Insert(30);
+            array.Insert(40);
+            array.Insert(50);
+            array.Insert(60);
+            array.Insert(70);
+            array.Insert(80);
+            //array.PrintArray();
+
+            //Console.WriteLine(array.ElementAt(4));
+            System.Console.WriteLine("\n\n");
+
+            //array.RemoveAt(0);
+
+            array.PrintArray();
+            array.RemoveAt(4);
+            System.Console.WriteLine(array.IndexOf(60));
+
+            Console.ReadLine();
+
         }
     }
 }
