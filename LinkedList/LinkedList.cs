0fc5ab9ed896ebcd2facec8a75a6d0bc31338489
iraqoze <iraqoze.lionel>
2021-12-04 10:55:17 +0300

    LinkedList Implementation

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,173 @@
+using System;
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        //Node Class
+        private class Node
+        {
+            public int Value;
+            public Node Next;
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+        }
+
+        private Node First;
+        private Node Last;
+        public int Count;
+
+
+        //Adding First
+        public void AddFirst(int value)
+        {
+            var node = new Node(value);
+            if (First == null)
+                First = Last = node;
+            else
+            {
+                node.Next = First;
+                First = node;
+            }
+            Count++;
+        }
+
+        //Adding Last
+        public void AddLast(int value)
+        {
+            var node = new Node(value);
+            if (First == null)
+                First = Last = node;
+            else
+            {
+                Last.Next = node;
+                Last = node;
+            }
+            Count++;
+        }
+
+        //Index Of
+        public int IndexOf(int value)
+        {
+
+            var current = First;
+            int index = 0;
+            while (current != null)
+            {
+                if (current.Value == value)
+                    return index;
+
+                current = current.Next;
+                index++;
+            }
+            return -1;
+        }
+
+        //Contains
+        public bool Contains(int value)
+        {
+            if (IndexOf(value) == -1)
+                return false;
+            return true;
+        }
+        //Count
+        public int GetCount() => Count;
+
+        //Remove First
+        public void RemoveFirst()
+        {
+            if (First == null)
+                throw new Exception("Can't remove first on an Empty List");
+
+            var second = First.Next;
+            First = second;
+            Count--;
+
+
+        }
+        //Remove Last
+        public void RemoveLast()
+        {
+
+            if (First == null)
+                throw new Exception("Can't remove first on an Empty List");
+
+            var current = First;
+            while (current != null)
+            {
+                if (current.Next == Last)
+                    break;
+                current = current.Next;
+            }
+            Last = current;
+            Last.Next = null;
+            Count--;
+
+        }
+
+        //Kth Element
+        public int KthNode(int distance)
+        {
+
+            if (distance < 0 || distance > Count)
+                throw new Exception("Invalid Distance");
+
+            var current = First;
+            var kthNode = First;
+            for (int i = 0; i < distance; i++)
+                current = current.Next;
+            while (current != null)
+            {
+                current = current.Next;
+                kthNode = kthNode.Next;
+            }
+            return kthNode.Value;
+        }
+
+        //ToArray
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+            var current = First;
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = current.Value;
+                current = current.Next;
+            }
+            return array;
+        }
+
+        //Clear the List
+        public void Clear()
+        {
+            First = Last = null;
+            Count = 0;
+        }
+
+        //Reverse LinkedLis
+        public void Reverse()
+        {
+            if (First == null)
+                throw new Exception("Can't Reverse an empty list");
+
+            var current = First;
+            Node temp = null;
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = temp;
+                temp = current;
+                current = next;
+            }
+
+            var f = First;
+            First = Last;
+            Last = f;
+            Last.Next = null;
+        }
+    }
+
+
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            System.Console.WriteLine("ADDING FIRST/ LAST\n");
+            linkedList.AddLast(10);
+            linkedList.AddLast(20);
+            linkedList.AddLast(30);
+            linkedList.AddLast(40);
+            linkedList.AddLast(50);
+
+            System.Console.WriteLine("\nINDEX OF\n");
+            Console.WriteLine(linkedList.IndexOf(40));
+
+            System.Console.WriteLine("\nCONTAINS\n");
+            System.Console.WriteLine(linkedList.Contains(20));
+            Console.WriteLine(linkedList.Contains(70));
+
+            System.Console.WriteLine("\nREMOVE FIRST\n");
+            // linkedList.RemoveFirst();
+
+            System.Console.WriteLine("\nREMOVE FIRST\n");
+            // linkedList.RemoveLast();
+
+            System.Console.WriteLine("\nKTH NODE\n");
+            Console.WriteLine(linkedList.KthNode(2));
+
+            System.Console.WriteLine("\nTO ARRAY\n");
+            var array = linkedList.ToArray();
+            foreach (var value in array)
+                Console.WriteLine(value);
+
+            System.Console.WriteLine("CLEAR THE LIST\n");
+            //linkedList.Clear();
+
+            linkedList.Reverse();
+            Console.ReadLine();
+        }
+    }
+}
