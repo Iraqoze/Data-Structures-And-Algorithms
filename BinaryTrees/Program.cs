0149c5ac4bd753ac4aebe7933aef80955023d643
iraqoze <iraqoze.lionel>
2021-12-12 00:01:23 +0300

    Binary Trees Implementation

---
+++ BinaryTrees/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BinaryTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree bst1 = new Tree();
+
+            bst1.Insert(30);
+            bst1.Insert(10);
+            bst1.Insert(5);
+            bst1.Insert(20);
+            bst1.Insert(15);
+            bst1.Insert(26);
+            bst1.Insert(1);
+            bst1.Insert(8);
+            bst1.Insert(50);
+            bst1.Insert(45);
+            bst1.Insert(60);
+            bst1.Insert(55);
+            bst1.Insert(70);
+
+            Tree bst = new Tree();
+
+            bst.Insert(30);
+            bst.Insert(10);
+            bst.Insert(5);
+            bst.Insert(20);
+            bst.Insert(15);
+            bst.Insert(26);
+            bst.Insert(1);
+            bst.Insert(8);
+            bst.Insert(50);
+            bst.Insert(45);
+            bst.Insert(60);
+            bst.Insert(55);
+            bst.Insert(80);
+            //System.Console.WriteLine(bst.Find(60));
+            //System.Console.WriteLine(bst.Find(10));
+
+            Console.WriteLine("\nPREORDER TRAVERSAL\n");
+            bst1.PreOrderTraversal();
+            Console.WriteLine("\nINORDER TRAVERSAL\n");
+            bst1.InOrderTraversal();
+            Console.WriteLine("\nPOSTORDER TRAVERSAL\n");
+            bst1.PostOrderTraversal();
+
+            Console.WriteLine("\nHEIGHT OF THE TREE\n");
+            System.Console.WriteLine(bst1.Height());
+            System.Console.WriteLine("\nTHE MINIMUM VALUE\n");
+            System.Console.WriteLine(bst1.Min());
+            System.Console.WriteLine("\nTHE MAXIMUM VALUE\n");
+            System.Console.WriteLine(bst1.Max());
+            System.Console.WriteLine("\nIS EQUAL\n");
+            System.Console.WriteLine(bst1.Equals(bst));
+            System.Console.WriteLine("\nVALIDATE BST\n");
+            System.Console.WriteLine(bst1.IsBinarySearchTree());
+
+            System.Console.WriteLine("\nNODES AT DISTANCE D\n");
+            bst1.GetNodesAtDistance(3);
+
+            System.Console.WriteLine("\nBREADTH FIRST TRAVERSAL\n");
+            bst1.BreadthFirstTraversal();
+
+            Console.WriteLine("\nCOUNT\n");
+            Console.WriteLine(bst1.GetCount());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BinaryTrees/Tree.cs
@@ -0,0 +1,250 @@
+using System;
+using System.Collections.Generic;
+
+namespace BinaryTrees
+{
+    public class Tree
+    {
+        public class Node
+        {
+            public int Value;
+            public Node LeftChild;
+            public Node RightChild;
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+        }
+
+        public Node Root;
+        int Count;
+
+        //Insert
+
+        public void Insert(int value)
+        {
+            var node = new Node(value);
+            if (Root == null)
+            {
+                Root = node;
+                Count++;
+                return;
+            }
+
+            var current = Root;
+            while (true)
+            {
+                if (value < current.Value)
+                {
+                    if (current.LeftChild == null)
+                    {
+                        current.LeftChild = node;
+                        break;
+                    }
+                    current = current.LeftChild;
+
+                }
+                else
+                {
+                    if (current.RightChild == null)
+                    {
+                        current.RightChild = node;
+                        break;
+                    }
+                    current = current.RightChild;
+                }
+
+            }
+            Count++;
+
+        }
+
+        //Find
+        public bool Find(int value)
+        {
+            if (Root == null)
+                return false;
+            var current = Root;
+            while (current != null)
+            {
+
+                if (value < current.Value)
+                    current = current.LeftChild;
+                else if (value > current.Value)
+                    current = current.RightChild;
+                else
+                    return true;
+            }
+            return false;
+
+        }
+
+        //PRE-Order Traversal: ROOT, LEFT, RIGHT 
+        public void PreOrderTraversal()
+        {
+            PreOrderTraversal(Root);
+        }
+        private void PreOrderTraversal(Node root)
+        {
+            if (root == null)
+                return;
+
+            //Print Root
+            Console.WriteLine(root.Value);
+
+            PreOrderTraversal(root.LeftChild);
+            PreOrderTraversal(root.RightChild);
+
+        }
+        //IN-Order Traversal: LEFT, ROOT, RIGHT 
+        public void InOrderTraversal()
+        {
+            InOrderTraversal(Root);
+        }
+        private void InOrderTraversal(Node root)
+        {
+            if (root == null)
+                return;
+            InOrderTraversal(root.LeftChild);
+            Console.WriteLine(root.Value);
+            InOrderTraversal(root.RightChild);
+
+        }
+
+        //POST-Order Traversal: LEFT, RIGHT, ROOT
+        public void PostOrderTraversal()
+        {
+            PostOrderTraversal(Root);
+        }
+        private void PostOrderTraversal(Node root)
+        {
+            if (root == null)
+                return;
+
+            PostOrderTraversal(root.LeftChild);
+            PostOrderTraversal(root.RightChild);
+
+            //Print Root
+            Console.WriteLine(root.Value);
+
+        }
+
+        //Height of the Tree
+        public int Height()
+        {
+            if (Root == null)
+                return -1;
+            return Height(Root);
+        }
+        private int Height(Node root)
+        {
+            if (root == null)
+                return -1;
+            if (IsLeaf(root))
+                return 0;
+
+            return 1 + Math.Max(Height(root.LeftChild), Height(root.RightChild));
+        }
+
+
+
+        //Minimum Value in The Tree
+        public int Min() => Min(Root);
+        private int Min(Node root)
+        {
+            if (IsLeaf(root))
+                return root.Value;
+            var left = Min(root.LeftChild);
+            var right = Min(root.RightChild);
+            return Math.Min(root.Value, Math.Min(left, right));
+        }
+
+        //Maximum Value in The Tree
+        public int Max() => Max(Root);
+        private int Max(Node root)
+        {
+
+            if (IsLeaf(root))
+                return root.Value;
+            var left = Max(root.LeftChild);
+            var right = Max(root.RightChild);
+            return Math.Max(root.Value, Math.Max(left, right));
+        }
+
+        //Is Leaf Node
+        private bool IsLeaf(Node node) => node.LeftChild == null && node.RightChild == null;
+
+        //Equals
+        public bool Equals(Tree tree)
+        {
+            if (tree == null)
+                return false;
+            return Equals(Root, tree.Root);
+        }
+        private bool Equals(Node first, Node second)
+        {
+            if (first == null && second == null)
+                return true;
+            if (first != null && second != null)
+                return first.Value == second.Value
+                 && Equals(first.LeftChild, second.LeftChild)
+                 && Equals(first.RightChild, second.RightChild);
+
+            return false;
+
+        }
+
+        //Validate Binary Search Tree
+        public bool IsBinarySearchTree() => IsBinarySearchTree(Root, int.MinValue, int.MaxValue);
+        private bool IsBinarySearchTree(Node root, int min, int max)
+        {
+            if (root == null)
+                return true;
+            if (root.Value < min || root.Value > max)
+                return false;
+            return IsBinarySearchTree(root.LeftChild, min, root.Value - 1)
+            && IsBinarySearchTree(root.RightChild, root.Value + 1, max);
+
+        }
+
+        //Nodes At K Distance from the Root Node
+        public void GetNodesAtDistance(int distance)
+        {
+            if (Math.Abs(distance) > Height())
+                throw new Exception("Distance Greater Than Height Exception");
+            GetNodesAtDistance(Root, Math.Abs(distance));
+        }
+        private void GetNodesAtDistance(Node root, int distance)
+        {
+            if (root == null)
+                return;
+
+            if (distance == 0)
+            {
+                Console.WriteLine(root.Value);
+                return;
+            }
+            GetNodesAtDistance(root.LeftChild, distance - 1);
+            GetNodesAtDistance(root.RightChild, distance - 1);
+        }
+
+        //LEVEL ORDER TRAVERSAL/BREADTH FIRST TRAVERSAL
+        public void BreadthFirstTraversal()
+        {
+            BreadthFirstTraversal(Root);
+        }
+        private void BreadthFirstTraversal(Node root)
+        {
+            if (root == null)
+                return;
+            for (int i = 0; i <= Height(); i++)
+                GetNodesAtDistance(i);
+
+        }
+
+        //COUNT
+        public int GetCount() => Count;
+    }
+
+}
